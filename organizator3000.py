a9e84010ba72b58a012c877db0c8c2a24ec63eea
marcelo-fm <foschiera.marcelom@gmail.com>
2022-04-06 10:12:17 -0300

    Update organizator3000.py

--- organizator3000.py
+++ organizator3000.py
@@ -1,3 +1,15 @@
 
 import os, shutil
 
+#print(os.getcwd())
+
+def lista_arquivos(path=os.getcwd()):
+    extensoes = {}
+    arquivos = os.listdir(path)
+    folder = []
+    for i in range(len(arquivos)):
+        if os.path.isdir(arquivos[i]):
+            folder.append(arquivos[i])
+            del arquivos[i]
+            
+
