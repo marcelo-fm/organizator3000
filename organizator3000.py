d4f1e455afe0a18eef402e79e5e72e0031968c06
marcelo-fm <foschiera.marcelom@gmail.com>
2022-04-06 09:36:54 -0300

    Update organizator3000.py

--- organizator3000.py
+++ organizator3000.py
@@ -1,11 +1,2 @@
 
 
-class Organizator:
-    def __init__(self, path) -> None:
-        self.path = path
-    
-    def print_path(self):
-        print(self.path)
-
-o = Organizator("C:/users/m_fos/Documents")
-o.print_path()
